e1c15e55c2be3c403f883b2d6d098a05859bd325
Nishant Sinha <nishnha@gmail.com>
2016-10-29 17:28:41 -0400

    website up

--- js/arcgis.js
+++ js/arcgis.js
@@ -2,6 +2,10 @@ var map;
 require([
         "esri/map",
         "esri/dijit/LocateButton",
+        "esri/dijit/OverviewMap",
+        "esri/geometry/Point",
+        "esri/symbols/SimpleMarkerSymbol",
+        "esri/graphic",
         "dojo/domReady!"
     ], function(Map, LocateButton) {
     
@@ -12,9 +16,31 @@ require([
         });
 
         var geoLocate = new LocateButton({
-                map: map,
+            map: map,
                 highlightLocation: true
             }, "LocateButton"
         );
+
+        var overviewMap = new OverviewMap({
+            map: map,
+            visible: true
+        });
+
         geoLocate.startup();
+        overviewMap.startup();
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+                $('html, body').animate({
+                          scrollTop: target.offset().top
+                        }, 1000);
+                return false;
+              }
+      }
+    });
 });
