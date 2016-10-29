b54f9cad3eb82123e38d57dec86cae7223148d72
Nishant Sinha <nishnha@gmail.com>
2016-10-29 14:23:45 -0400

    working map, testing location

--- js/arcgis.js
+++ js/arcgis.js
@@ -1,1 +1,20 @@
-var map = new Map("map",{basemap: "topo",center: [-122.45,37.75],zoom: 13,sliderStyle: "small"});
+var map;
+require([
+        "esri/map",
+        "esri/dijit/LocateButton",
+        "dojo/domReady!"
+    ], function(Map, LocateButton) {
+    
+        map = new Map("map", {
+            basemap: "streets", //streets, satellite, hybrid, topo
+            center: [-79.046761, 35.904613], // longitude, latitude
+            zoom: 16
+        });
+
+        var geoLocate = new LocateButton({
+                map: map,
+                highlightLocation: true
+            }, "LocateButton"
+        );
+        geoLocate.startup();
+});
